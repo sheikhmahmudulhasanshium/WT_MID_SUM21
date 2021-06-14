f9845b533ab427ccd23d2c1f49b34f3d22fa5f5f
Sheikh Mahmudul Hasan Shium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2021-06-14 11:03:48 +0600

    task -5 modified

--- Lab-3/problem-5.php
+++ Lab-3/problem-5.php
@@ -1,3 +1,27 @@
 <?php
-    
+  $backColor="green";
+  $fontColor="red";
+  $imageWidth="80px";
+  $imageHeight="80px";
 ?>
+<html>
+    <body>
+       <table>
+        <tr>
+          <td>
+            <img src="aiub_logo.jpg" alt=logo" height="$imageHeight" width="$imageWidth">
+          </td>
+          <td>
+            <h2>
+              American International <br>
+               University-
+                <span style="background-color:green; color:red">
+                Bangladesh
+              </span>
+              
+            </h2>
+          </td>
+        </tr>
+       </table>
+    </body>
+</html>
